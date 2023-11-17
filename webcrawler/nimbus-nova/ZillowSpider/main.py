f02446b05ab9d6604f069fb947ecae1151e2146e
Andyfanfan <985897924@qq.com>
2023-11-16 19:56:07 -0800

    remove scrapy

--- webcrawler/nimbus-nova/ZillowSpider/main.py
+++
@@ -1,8 +0,0 @@
-from scrapy.cmdline import execute
-
-import sys
-import os
-
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-# print(sys.path)
-execute("scrapy crawl realtor".split(' '))
